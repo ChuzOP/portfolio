e0f81637169f425149e3f882256f9b372beb282c
Jesus García <demiguelgarcia2003@gmail.com>
2024-09-04 17:00:53 -0600

    UPDATE: cambios a la logica de proyectos

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -16,30 +16,29 @@ export interface Project {
   githubLink?: string;
   figmaLink?: string;
   liveLink?: string;
+  href: string;
 }
 
 export const projects: Project[] = [
   {
-    name: "Proyecto 1",
+    name: "ERP web",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis quod laboriosam quas quos doloribus err",
-    technologies: [1, 2],
-    githubLink: "https://github.com/ChuzOP/portfolio",
-    figmaLink: "https://github.com/ChuzOP/portfolio",
+      "Sistema full-stack multi usuario con gestión de inventario, punto de venta y generador de reportes PDF",
+    technologies: [1, 2, 5, 6, 7, 9],
+    href: "/proyects/erp",
   },
   {
-    name: "Proyecto 2",
+    name: "Sistema Contable",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis quod laboriosam quas quos doloribus err",
-    technologies: [2, 3],
-    figmaLink: "https://github.com/ChuzOP/portfolio",
+      "Gestor de facturas de compras y ventas alimentado por archivos XML y CSV y generando reportes contables como libros de compras, ventas, diario, mayor y balance",
+      technologies: [6, 7, 8, 9],
+    href: "/proyects/contable",
   },
   {
-    name: "Proyecto 3",
+    name: "App Delivery Food",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis quod laboriosam quas quos doloribus err",
-    technologies: [4, 5, 6, 7, 8],
-    liveLink: "https://github.com/ChuzOP/portfolio",
-    figmaLink: "https://github.com/ChuzOP/portfolio",
+      "Aplicación mobile multiplataforma para cocineros y deilveries",
+    technologies: [4, 5, 9],
+    href: "/proyects/app-delivery",
   },
 ];
